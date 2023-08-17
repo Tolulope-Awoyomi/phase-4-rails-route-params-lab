945f49d22db915a6621ab7a468d175996e1ef1cd
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-17 19:50:58 -0400

    Code lab solution

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name].present?
+      search_term = params[:name].downcase
+      students = Student.where("LOWER(first_name) LIKE ? OR LOWER(last_name) LIKE ?", "%#{search_term}%", "%#{search_term}%")
+    else
+      students = Student.all
+    end
+  
+      render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
